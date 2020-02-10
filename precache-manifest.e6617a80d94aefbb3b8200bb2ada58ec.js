bd63e3836abb7e88d109a9ec2ef842c72be97ea3
VladSWEED <svidvlad15@gmail.com>
2020-02-10 14:29:31 +0300

    Updates

---
+++ precache-manifest.e6617a80d94aefbb3b8200bb2ada58ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203332f48afa9fa0143f29dc23017e82",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e4a7a3f07a30ee029608",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad8e8dccf92391a62d32",
+    "url": "/portfolio/static/js/2.50fdd86e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.50fdd86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4a7a3f07a30ee029608",
+    "url": "/portfolio/static/js/main.5546011b.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
